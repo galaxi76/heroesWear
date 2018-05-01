c7fa8d5f2daa3abb8d3336febf19f981ef950680
Shirly Kadosh <shirlyk@gett.com>
2018-05-01 21:49:00 +0300

    Add sounds

--- HeroesMakers/app/src/main/java/app/heroeswear/com/heroesmakers/login/Activities/AreYouOkActivity.java
+++ HeroesMakers/app/src/main/java/app/heroeswear/com/heroesmakers/login/Activities/AreYouOkActivity.java
@@ -1,11 +1,17 @@
 package app.heroeswear.com.heroesmakers.login.Activities;
 
+import android.content.ContentResolver;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.os.PowerManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+
+import app.heroeswear.com.heroesfb.Logger;
 import app.heroeswear.com.heroesmakers.R;
 
 public class AreYouOkActivity extends AppCompatActivity {
@@ -17,6 +23,7 @@ public class AreYouOkActivity extends AppCompatActivity {
 
         //setShowWhenLocked(true);
         //setTurnScreenOn(true);
+        playNotificationSound();
     }
 
     @Override
@@ -24,12 +31,21 @@ public class AreYouOkActivity extends AppCompatActivity {
         //this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
         this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN
                 | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+                | WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         super.onAttachedToWindow();
-        //| WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
+    }
 
-//        final Window win= getWindow();
-//        win.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-//        win.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+    public void playNotificationSound() {
+        try {
+            Uri alarmSound = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE
+                    + "://" + getPackageName() + "/raw/push_notification_sound");
+            Ringtone r = RingtoneManager.getRingtone(this, alarmSound);
+            r.play();
+        } catch (Exception e) {
+            Logger.Companion.e(e.getMessage());
+        }
     }
 }
--- HeroesMakers/app/src/main/java/app/heroeswear/com/heroesmakers/login/fcm/HeroesFirebaseMessagingService.java
+++ HeroesMakers/app/src/main/java/app/heroeswear/com/heroesmakers/login/fcm/HeroesFirebaseMessagingService.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import app.heroeswear.com.heroesfb.Logger;
 import app.heroeswear.com.heroesmakers.login.Activities.AreYouOkActivity;
 import app.heroeswear.com.heroesmakers.login.enums.NotificationChannelType;
 import app.heroeswear.com.heroesmakers.login.utils.NotificationFactory;
@@ -21,12 +20,9 @@ public class HeroesFirebaseMessagingService extends FirebaseMessagingService {
     public void handleIntent(Intent intent) {
         super.handleIntent(intent);
 
-//        RemoteMessage.Notification notification = remoteMessage.getNotification();
-//        Logger.Companion.d("Title: " + remoteMessage.getNotification().getTitle());
-//        NotificationFactory.build(this, notification.getTitle(), notification.getBody(), NotificationChannelType.HEART_RATE);
-
-        Intent in = new Intent(this, AreYouOkActivity.class);
-        in.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(in);
+        NotificationFactory.build(this, "Hi", "Are you ok?", NotificationChannelType.HEART_RATE_ALARTS);
+        Intent activityIntent = new Intent(this, AreYouOkActivity.class);
+        activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(activityIntent);
     }
 }

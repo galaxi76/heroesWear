5d7c34b8b16f38a8b74ad3a74dd0158b469d42cc
livnat avikasis <livnata@gett.com>
2018-05-02 11:17:21 +0300

    Implemented Activity Navigation

--- HeroesMakers/app/src/main/java/app/heroeswear/com/heroesmakers/login/Activities/AreYouOkActivity.java
+++ HeroesMakers/app/src/main/java/app/heroeswear/com/heroesmakers/login/Activities/AreYouOkActivity.java
@@ -1,27 +1,50 @@
 package app.heroeswear.com.heroesmakers.login.Activities;
 
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.os.PowerManager;
-import android.support.annotation.Nullable;
+
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import app.heroeswear.com.heroesfb.Logger;
 import app.heroeswear.com.heroesmakers.R;
 
 public class AreYouOkActivity extends AppCompatActivity {
 
+    private Button btn_notOk;
+    private Button btn_Ok;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_are_you_ok);
-    }
 
+        btn_notOk = (Button) findViewById(R.id.btn_not_ok);
+        btn_notOk.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                launchActivity();
+            }
+        });
+
+        btn_Ok = (Button) findViewById(R.id.btn_ok);
+        btn_Ok.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                moveTaskToBack(true);
+            }
+        });
+
+        //setShowWhenLocked(true);
+        //setTurnScreenOn(true);
+    }
+    private void launchActivity(){
+        Intent intent = new Intent(AreYouOkActivity.this,SolutionListActivity.class);
+        startActivity(intent);
+    }
     @Override
     public void onAttachedToWindow() {
         //this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
